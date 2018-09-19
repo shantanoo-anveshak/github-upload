3bdfd11a8e47e5cfd308a86592eef7fbf4d37474
Shantanoo H <shantanoo.harmalkar@anveshak.com>
2018-09-19 16:56:59 +0530

    Demo change

--- celery.py
+++ celery.py
@@ -4,7 +4,7 @@ from celery import Celery
 #creating instance of the celery where broker is broker_url (i.e rabbitmq) in format : -> transport://user:password@hostname:port/virtual_host a
 # "transport" for rabbitmq is "amqp"
 #first argument to Celery is project package name
-#third argument backend is backend url and rpc means sending the results back as AMQP messages
+#third argument backend is backend url and rpc means sending the results back as AMQP messages mla ky kalle nahi
 
 app = Celery('test_celery',
              broker='amqp://shanty:shanty123@localhost/shanty_vhost',
