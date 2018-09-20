2116ce5680654b66bacced21fe7906cbeb4f66ed
Chetan T <chetan.tamboli@anveshak.com>
2018-09-20 10:20:44 +0530

    changed sleep time to 10

--- tasks.py
+++ tasks.py
@@ -6,7 +6,7 @@ import time
 @app.task
 def longtime_add(x, y):
     print('long time task begins')
-    # sleep 5 seconds
-    time.sleep(5)
+    # sleep 10 seconds
+    time.sleep(10)
     print('long time task finished')
     return x + y
