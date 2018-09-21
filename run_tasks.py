d70a107793dc432b2947ad253758aedc01846918
Shantanoo H <shantanoo.harmalkar@anveshak.com>
2018-09-21 14:35:56 +0530

    used subract function

--- run_tasks.py
+++ run_tasks.py
@@ -4,10 +4,15 @@ import time
 if __name__ == '__main__':
     result = longtime_add.delay(1,2)
     # at this time, our task is not finished, so it will return False
-    print ('Task finished? ', result.ready())
-    print ('Task result: ', result.result)
+    print ('Add Task finished? ', result.ready())
+    print ('Add Task result: ', result.result)
     # sleep 10 seconds to ensure the task has been finished
     time.sleep(10)
     # now the task should be finished and ready method will return True
-    print ('Task finished? ', result.ready())
-    print ('Task result: ', result.result)
+    print ('Add Task finished? ', result.ready())
+    print ('Add Task result: ', result.result)
+    result1 = longtime_subtract.delay(5,2)
+    print('Subtract task finished?', result1.ready())
+    if(result1.ready()):
+	print('Subtract task result: ', result1.result)
+
